0565e34cec6e22b30e04b049f1b42f25eb9cd82f
Raul Jaco <jacochaka@gmail.com>
2021-07-07 15:14:16 -0600

    deploy commit

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
 const app = express();
+const app = express();
+app.use(requireHTTPS);
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/web-cashflow'));
+app.use(express.static('./dist/web-cashflow'));
 
-// Send all requests to index.html
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/web-cashflow/index.html'));
+    res.sendFile('index.html', { root: 'dist/web-cashflow' }
+    );
 });
 
 // default Heroku port

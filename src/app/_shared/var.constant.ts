64032e735b9b255aa854dbcab8214dc0ca123483
RJaco <jacochaka@gmail.com>
2025-01-09 22:11:47 -0600

    constants updated

--- src/app/_shared/var.constant.ts
+++ src/app/_shared/var.constant.ts
@@ -1,1 +1,4 @@
 export const HOST = 'https://cashflow-rest.herokuapp.com';
+export const TOKEN_AUTH_USERNAME = 'cashflowapp';
+export const TOKEN_AUTH_PASSWORD = 'RJ$tar0510*'
+export const TOKEN_NAME = 'access_token';
